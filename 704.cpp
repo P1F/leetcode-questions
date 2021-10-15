53c2413160439c8fd539f9f9eb072bdceefce0e3
P1F <rtsouza15@gmail.com>
2021-10-14 23:30:04 -0400

    comment corrections

--- 704.cpp
+++ 704.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 /*
-Problem:
+Problem: 704. Binary Search
 
 Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 
 You must write an algorithm with O(log n) runtime complexity.
 ----------------------------------------------------------------------------------
-/*Example 1:
+Example 1:
 
 Input: nums = [-1,0,3,5,9,12], target = 9
 Output: 4
