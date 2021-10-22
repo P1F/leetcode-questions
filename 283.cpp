88832fa54fbd07dac0c93b8e7ab8ed06cfdf7009
P1F <rtsouza15@gmail.com>
2021-10-22 19:33:34 -0400

    14 days study plan - day 3 question 1

---
+++ 283.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+/*
+283. Move Zeroes
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+----------------------------------------------------------------------------------
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+--------------------------------------------------------------------------------- 
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+---------------------------------------------------------------------------------
+Follow up: Could you minimize the total number of operations done?
+*/
+
+// FIRST TRY
+/*
+    ANALYSIS:
+    Time complexity: O(n)
+    Space complexity: O(n)
+*/
+// class Solution {
+// public:
+//     void swap(vector<int>&nums, int i, int j){
+//         int aux = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = aux;
+//     }
+
+//     void moveZeroes(vector<int>& nums) {
+//         int l = nums.size();
+//         if (l == 0 || l == 1) return;
+        
+//         queue<int> q;
+//         for (int i = 0; i < l; i++){
+//             if (nums[i] != 0 && !q.empty()){
+//                 swap(nums, i, q.front());
+//                 q.pop();
+//             }
+//             if (nums[i] == 0)
+//                 q.push(i);   
+//         }
+//     }
+// };
+
+// SECOND TRY
+/*
+    ANALYSIS:
+    Time complexity: O(n)
+    Space complexity: O(1)
+*/
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        int l = nums.size();
+        for (int i = 0; i < l; i++){
+            if (nums[i]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        for (; j < l; j++)
+            nums[j] = 0;
+    }
+};
+
+int main(){
+    vector<int> nums1 = {0,1,0,3,12};
+    vector<int> nums2 = {0};
+
+    Solution().moveZeroes(nums1);
+    Solution().moveZeroes(nums2);
+
+    cout << "nums1: ";
+    for (const auto& e : nums1) {
+        cout << e << " ";
+    }
+    cout << endl << "nums2: ";
+    for (const auto& e : nums2) {
+        cout << e << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
